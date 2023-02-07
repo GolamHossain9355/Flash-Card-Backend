5dd7a46fe641d14bd842b8b68cdb3711c938f61b
Golam <rabby29hossain@gmail.com>
2023-02-07 16:07:09 -0500

    changes

--- src/utils/map-properties.js
+++ src/utils/map-properties.js
@@ -3,7 +3,7 @@ const lodash = require("lodash");
 function mapProperties(configuration) {
   return (data) => {
     if (data) {
-      return Object.entries(data).reduce((accumulator, [key, value]) => {
+      return   Object.entries(data).reduce((accumulator, [key, value]) => {
         return lodash.set(accumulator, configuration[key] || key, value);
       }, {});
     }
